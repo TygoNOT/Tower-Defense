ea4566ff653eb951a1f709bcd087dbbe69208ec9
CookieLoverDev <ukhodjamov@gmail.com>
2024-05-01 02:33:26 +0200

    Laser Tower Fix

--- Tower Defense/Assets/Script/Towers/LaserTower.cs
+++ Tower Defense/Assets/Script/Towers/LaserTower.cs
@@ -5,80 +5,43 @@ using UnityEngine;
 
 public class LaserTower : TurretLogical
 {
-    private float defaultDamage = 0.25f;
+    private Transform lastHit;
+    private Bullet laser;
 
     private void Start()
     {
-        Bullet laser = bulletPrefab.GetComponent<Bullet>();
+        laser = bulletPrefab.GetComponent<Bullet>();
         if (laser != null)
         {
-             defaultDamage = laser.bulletDamage;
-             Debug.Log(defaultDamage);
+             laser.bulletDamage = 0.25f;
+             Debug.Log(laser.bulletDamage);
         }
+
+        lastHit = target;
     }
 
     protected override void Shoot()
     {
         Bullet laser = bulletPrefab.GetComponent<Bullet>();
-        laser.bulletDamage = defaultDamage;
+        Debug.Log("Target: " + target);
+        Debug.Log("Last Hit: " + lastHit);
 
-        //Everytime it shoots, damage value increased in 2 times
-        if(target != null)
+        if (target != lastHit)
         {
-            if (laser != null)
-            {
-                defaultDamage *= 2f;
-                Debug.Log(defaultDamage);
-
-                Enemy enemy = gameObject.GetComponent<Enemy>();
-                //if enemy died or left the range, damage value should reset to default value
-                if (target == null)
-                {
-                    ResetDamage();
-                }
-                else
-                {
-                    Debug.Log("The damage value did not reset");
-                }
-            }
-            base.Shoot();
+            ResetDamage();
         }
-        
+
+        base.Shoot();
+        Debug.Log("Current damage is: " + laser.bulletDamage);
+        laser.bulletDamage *= 2f;
+        lastHit = target;
     }
+
 
     //Reset damage method
     private void ResetDamage()
     {
-        Bullet laser = bulletPrefab.GetComponent<Bullet>();
-        if (laser != null)
-        {
-            laser.bulletDamage = 0.25f;
-            Debug.Log("Bullet damage has been reset to default: " + laser.bulletDamage);
-        }
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-    }
-
-    protected override void RotateTowardsTarget()
-    {
-        base.RotateTowardsTarget();
-    }
-
-    protected override void FindTarget()
-    {
-        base.FindTarget();
-    }
-
-    protected override bool CheckTargetIsInRange()
-    {
-        return base.CheckTargetIsInRange();
-    }
-
-    protected override void OnDrawGizmosSelected()
-    {
-        base.OnDrawGizmosSelected();
+        laser.bulletDamage = 0.25f;
+        Debug.Log("Bullet damage has been reset to default");
     }
 }

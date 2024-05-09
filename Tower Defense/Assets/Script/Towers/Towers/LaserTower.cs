d9d56afc84ebd0bcc0d19a015911ebaa702c79ae
skywallkerr <147839321+shadowraazee@users.noreply.github.com>
2024-05-09 19:55:12 +0200

    Added laser beam to LaserTower
    added laser beam for the laser tower

--- Tower Defense/Assets/Script/Towers/Towers/LaserTower.cs
+++ Tower Defense/Assets/Script/Towers/Towers/LaserTower.cs
@@ -7,17 +7,25 @@ public class LaserTower : TurretLogical
 {
     private Transform lastHit;
     private Bullet laser;
+    private GameObject laserObj;
+    private LineController laserBeam;
+
+
+    [SerializeField]
+    private Transform origin;
+    [SerializeField]
+    public GameObject laserPrefab;
 
     private void Start()
     {
         laser = bulletPrefab.GetComponent<Bullet>();
         if (laser != null)
         {
-             laser.bulletDamage = 0.25f;
-             Debug.Log(laser.bulletDamage);
+            laser.bulletDamage = 0.25f;
+            Debug.Log(laser.bulletDamage);
         }
 
-        lastHit = target;
+        lastHit = target;   
     }
 
     protected override void Shoot()
@@ -35,13 +43,87 @@ public class LaserTower : TurretLogical
         Debug.Log("Current damage is: " + laser.bulletDamage);
         laser.bulletDamage *= 2f;
         lastHit = target;
+
+        if (target != null && laserBeam == null)
+        {
+            GameObject laserObj = Instantiate(laserPrefab, origin.position, Quaternion.identity);
+            laserBeam = laserObj.GetComponent<LineController>();
+            laserBeam.SetStartPosition(origin.position);
+            laserBeam.SetEndPosition(target.position);
+            StartCoroutine(UpdateLaserBeamCoroutine(target.position, laserBeam));
+        }
+
     }
 
+    protected override void Update()
+    {
+        base.Update();
 
-    //Reset damage method
+        if (target != null)
+        {
+            if (lastHit != target || laserBeam == null)
+            {
+                if (laserObj != null)
+                {
+                    laserObj.SetActive(false);
+                }
+
+                if (laserBeam != null)
+                {
+                    Destroy(laserBeam.gameObject);
+                    laserBeam = null;
+                }
+
+                laserObj = Instantiate(laserPrefab, origin.position, Quaternion.identity);
+                laserBeam = laserObj.GetComponent<LineController>();
+            }
+
+            if (laserBeam != null)
+            {
+                laserBeam.SetStartPosition(origin.position);
+                laserBeam.SetEndPosition(target.position);
+                StartCoroutine(UpdateLaserBeamCoroutine(target.position, laserBeam));
+            }
+        }
+        else
+        {
+            if (laserBeam != null)
+            {
+                Destroy(laserBeam.gameObject);
+                laserBeam = null;
+            }
+
+            if (laserObj != null)
+            {
+                laserObj.SetActive(false);
+            }
+        }
+    }
     private void ResetDamage()
     {
         laser.bulletDamage = 0.25f;
         Debug.Log("Bullet damage has been reset to default");
+    }
+
+    
+    private IEnumerator UpdateLaserBeamCoroutine(Vector3 targetPosition, LineController lineController)
+    {
+        float elapsedTime = 0f;
+        float duration = 0.1f;
+
+        Vector3 startPosition = origin.position;
+        Vector3 endPosition = targetPosition;
+
+        while (elapsedTime < duration)
+        {
+            Vector3 currentPosition = Vector3.Lerp(startPosition, endPosition, elapsedTime / duration);
+            lineController.SetEndPosition(currentPosition);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        lineController.SetEndPosition(endPosition);
     }
 }

861052a2770ba0730ab7a8ef2308bf21bebf5774
CookieLoverDev <ukhodjamov@gmail.com>
2024-06-06 12:47:51 +0200

    Major Update
    -All 3 levels are completely finished and playable
    -Added map borders and camera is restricted go over them

--- Tower Defense/Assets/Script/Game Logic/CameraMovement.cs
+++ Tower Defense/Assets/Script/Game Logic/CameraMovement.cs
@@ -1,49 +1,63 @@
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
-    public class CameraMovement : MonoBehaviour
+public class CameraMovement : MonoBehaviour
+{
+    Vector3 hitPosition = Vector3.zero;
+    Vector3 currentPosition = Vector3.zero;
+    Vector3 cameraPosition = Vector3.zero;
+
+    public GameObject Point;
+    public BuildManager buildManager;
+
+    [Header("Map Borders")]
+    [SerializeField] private float minX;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxX;
+    [SerializeField] private float maxY;
+
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 hitPosition = Vector3.zero;
-        Vector3 currentPosition = Vector3.zero;
-        Vector3 cameraPosition = Vector3.zero;
+        if (LevelManager.main.gamePaused || LevelManager.main.gameOver)
+            return;
 
-        public GameObject Point;
-        public BuildManager buildManager;
-
-
-        // Update is called once per frame
-        void Update()
+        if (Input.GetMouseButtonDown(0))
         {
-            if (LevelManager.main.gamePaused || LevelManager.main.gameOver)
-                return;
-
-            if(Input.GetMouseButtonDown(0))
-            {
-                hitPosition = Input.mousePosition;
-                cameraPosition = transform.position;
-                Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);
-                Point = hit.transform.gameObject;
-            }
-
-            if (Input.GetMouseButton(0))
-            {
-                currentPosition = Input.mousePosition;
-                if (Point.CompareTag("Build Point"))
-                    return;
-                else
-                    DragTheCamera();
-            }
+            hitPosition = Input.mousePosition;
+            cameraPosition = transform.position;
+            Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);
+            Point = hit.transform.gameObject;
         }
-        private void DragTheCamera()
+
+        if (Input.GetMouseButton(0))
         {
-            Vector3 direction = Camera.main.ScreenToWorldPoint(currentPosition) - Camera.main.ScreenToWorldPoint(hitPosition);
-            direction *= -1;
-
-            Vector3 newPosition = cameraPosition + direction;
-            transform.position = newPosition;
-
+            currentPosition = Input.mousePosition;
+            if (Point.CompareTag("Build Point"))
+                return;
+            else
+                DragTheCamera();
         }
     }
+
+    private void LateUpdate()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+        float clampedY = Mathf.Clamp(transform.position.y, minY, maxY);
+
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+
+    private void DragTheCamera()
+    {
+        Vector3 direction = Camera.main.ScreenToWorldPoint(currentPosition) - Camera.main.ScreenToWorldPoint(hitPosition);
+        direction *= -1;
+
+        Vector3 newPosition = cameraPosition + direction;
+        transform.position = newPosition;
+
+    }
+}
